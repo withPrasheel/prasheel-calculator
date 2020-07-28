1426e7848ff7f5b1b33763477e3333a82276fbc4
Prasheel <prasheelnandwana3@gmail.com>
2020-07-28 16:18:17 +0530

    just working react app

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// import calc from './
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                    <App />
+            </div>
+
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
